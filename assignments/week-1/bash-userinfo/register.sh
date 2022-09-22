36dcd2666ccb6014b0d8aecd12b1c10e2489d624
Jonny Andino <66392799+bluephosphor@users.noreply.github.com>
2022-09-22 17:00:54 -0400

    more password security, extracting logic into functions

--- assignments/week-1/bash-userinfo/register.sh
+++ assignments/week-1/bash-userinfo/register.sh
@@ -1,28 +1,54 @@
 #!/bin/bash
 
-#take in a firstname, lastname, username, password and email
-echo 'Please enter your First and Last Name, separated by a space:'
-read firstname lastname
+#extract logic into functions for readability and reuseability
+get_names() {
+    echo 'Please enter your First and Last Name, separated by a space:'
+    read firstname lastname
+}
 
-echo 'Please register an email:'
-read email
-# validate that email does not exist in database
-# validate that email is formatted correctly
-echo 'Enter a new Username:'
-read username
-# validate that usernames do not already exist in database
-echo 'Enter a new Password:'
-read password
-# bonus: have password be invisible upon input? maybe ask twice
-# bonus: validate passwords to have a certain level of complexity eg: "one character and one number and one symbol... ect"
-# bonus: encrypt passwords
+get_email() {
+    echo 'Please register an email:'
+    read email
+    # validate that email does not exist in database
+    # validate that email is formatted correctly
+}
 
+get_username() {
+    echo 'Enter a new Username:'
+    read username
+    # validate that usernames do not already exist in database
+}
+
+get_password () {    
+    echo 'Enter a new Password: (input will be invisible)'
+    read -s password
+    echo 'Please re-enter password.'
+    read -s password2
+
+    if [ $password == $password2 ] 
+    then
+        echo 'sucessfully saved password'
+    else
+        echo 'Error: Passwords do not match. Please try again'
+        get_password
+    fi
+
+    # bonus: validate passwords to have a certain level of complexity eg: "one character and one number and one symbol... ect"
+    # bonus: encrypt passwords
+}
+
+# take in a firstname, lastname, username, password and email
+get_names
+get_username
+get_email
+get_password
+
+#validate that the info is correct
 echo "--------------------------------"
 echo "Name: ${firstname} ${lastname}"
 echo "Username: ${username}"
 echo "Password: *****"
 echo "email: ${email}"
 
-#validate that the info is correct
 
 #save this information to a csv file
