1b7cc4446e03199247d7b428f077c0d4bb2b97fe
Jonny Andino <66392799+bluephosphor@users.noreply.github.com>
2022-09-22 16:35:24 -0400

    basic outline, expanding on goals

--- assignments/week-1/bash-userinfo/register.sh
+++ assignments/week-1/bash-userinfo/register.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
 #take in a firstname, lastname, username, password and email
+echo 'Please enter your First and Last Name, separated by a space:'
+read firstname lastname
+
+echo 'Please register an email:'
+read email
+# validate that email does not exist in database
+# validate that email is formatted correctly
+echo 'Enter a new Username:'
+read username
+# validate that usernames do not already exist in database
+echo 'Enter a new Password:'
+read password
+# bonus: have password be invisible upon input? maybe ask twice
+# bonus: validate passwords to have a certain level of complexity eg: "one character and one number and one symbol... ect"
+# bonus: encrypt passwords
+
+echo "--------------------------------"
+echo "Name: ${firstname} ${lastname}"
+echo "Username: ${username}"
+echo "Password: *****"
+echo "email: ${email}"
 
 #validate that the info is correct
-
-#validate that usernames/emails do not already exist
-
-#bonus: validate passwords
 
 #save this information to a csv file
