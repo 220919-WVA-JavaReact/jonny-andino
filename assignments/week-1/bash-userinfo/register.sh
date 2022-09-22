2657085d8146d6e9d50e6d813bda3b4da129ac0a
Jonny Andino <66392799+bluephosphor@users.noreply.github.com>
2022-09-22 16:13:28 -0400

    create file, identify goals

---
+++ assignments/week-1/bash-userinfo/register.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#take in a firstname, lastname, username, password and email
+
+#validate that the info is correct
+
+#validate that usernames/emails do not already exist
+
+#bonus: validate passwords
+
+#save this information to a csv file
