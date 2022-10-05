b5c3e7eba05b818059ef45b5cb0edf95aa1c7453
Jonny Andino <66392799+bluephosphor@users.noreply.github.com>
2022-10-05 16:02:29 -0400

    more teacher stuff

--- assignments/idea/JavaFundamentals/src/main/java/com/revature/courses/dao/TeacherDAOImplPostgres.java
+++ assignments/idea/JavaFundamentals/src/main/java/com/revature/courses/dao/TeacherDAOImplPostgres.java
@@ -3,17 +3,49 @@ package com.revature.courses.dao;
 import com.revature.courses.models.Teacher;
 import com.revature.courses.util.ConnectionUtil;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class TeacherDAOImplPostgres implements TeacherDAO{
     @Override
     public Teacher getByUsername(String username) {
-        return null;
+        // to get by username we are going to use a prepared statement to help us
+        // prevent SQL injection
+
+        // first create a teacher object to store the information i pull back
+        Teacher teach = new Teacher();
+
+        // we're going to use a try with resources block to ensure we close the connection
+
+        try(Connection conn = ConnectionUtil.getConnection()){
+            String sql = "SELECT * FROM teachers WHERE username = ?";
+            //    data we want to insert into statement goes here ^
+
+            PreparedStatement stmt = conn.prepareStatement(sql);
+
+            //set the individual values for the question marks
+            stmt.setString(1,username);
+            ResultSet rs;
+
+            //since i am expecting one result i will handle it differently
+
+            if ((rs = stmt.executeQuery()) != null){
+                rs.next(); // iterate by one
+
+                int id          = rs.getInt("teacher_id");
+                String first    = rs.getString("first");
+                String last     = rs.getString("last");
+                String receivedUsername = rs.getString("username");
+                String password = rs.getString("password");
+
+                teach = new Teacher(id,first,last,receivedUsername,password);
+            }
+        } catch(SQLException e){
+            e.printStackTrace();
+        }
+
+        return teach;
     }
 
     @Override
