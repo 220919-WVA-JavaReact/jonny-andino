88da5a1a0ec2982d815b69c274a6b2afb7c59a0c
Jonny Andino <66392799+bluephosphor@users.noreply.github.com>
2022-10-11 14:28:55 -0400

    got servlet working for example

---
+++ misc/experimenting/src/main/java/org/example/SanityServlet.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+// whenever we want to create a servlet we will extend the HttpServlet
+public class SanityServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // we'll just add some println statements to check some info about the request
+        System.out.println("[LOG] - SanityServlet received a request at " + LocalDateTime.now());
+        System.out.println("[LOG] - Request URI: " + req.getRequestURI());
+        System.out.println("[LOG] - Request Method: " + req.getMethod());
+        System.out.println("[LOG] - Request Header, example" + req.getHeader("example"));
+
+        // we'll formulate a basic response to send back
+
+        // set the status code
+        resp.setStatus(200);
+        resp.setHeader("Content-type", "text/plain");
+        resp.setHeader("example-response-header", "some-example-value");
+        resp.getWriter().write("Hello from servlet!");
+    }
+}
